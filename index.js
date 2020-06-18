63157d5fea0e75a9e9d10ed07c8356ee5699fb60
Ziyad <ziyadkader@gmail.com>
2020-06-18 02:48:25 +0000

    init

---
+++ index.js
@@ -0,0 +1,87 @@
+const fs = require("fs")
+const process = require("process");
+const wrap = require("word-wrap");
+
+function getArguments(){
+	const allowedArgs = ["-html","-css","-js"];
+	const command = {
+		type: process.argv[2],
+		value:process.argv[3]
+	}
+	
+	if(!allowedArgs.includes(process.argv[2])){
+		console.log(`invalid option : ${process.argv[2]}`);
+		process.exit();
+	}
+	else{
+		command.type = command.type.substr(1);
+		return command
+	}
+}
+function displayCheat(type,title,cheatObj){
+	
+	if(cheatObj === undefined){
+		switch(type){
+			case 'html':
+				console.log("[\033[33;1mNOT FOUND\033[0m]" + ` <${title}> : not found`);
+				break;
+			case 'css':
+				console.log("[\033[33;1mNOT FOUND\033[0m] The CSS property that you requested doesn't exist/isn't indexed");
+				break;
+			case 'js':
+				console.log("[\033[33;1mNOT FOUND\033[0m]:" + `${title}`);
+				break;
+			default:
+				console.log("something went wrong");
+		}
+		process.exit();
+	}
+
+
+	let keys = Object.keys(cheatObj);
+	
+	let lineSeparator = "━".repeat(72);
+	let titleBar = "┏";
+	titleBar += lineSeparator
+	titleBar += "┓\n┃\u001b[33;1m "
+	titleBar += wrap(title,{width:60}).padEnd(71); 
+	titleBar += "\u001b[0m┃\n"
+	titleBar += "┣" + lineSeparator + "┫"
+	console.log(titleBar);
+	
+	// body section
+	for(let element in cheatObj) {
+		
+		let body = cheatObj[element];
+		console.log("┃" + " ".repeat("72") + "┃");
+		
+		console.log("┃ ● \033[4;33m" + element + "\033[0m" + "┃".padStart(70 - element.length));
+		// console.log("┃");
+		body = wrap(body,{width:65}).split("\n");
+		console.log("┃" + " ".repeat("72") + "┃");
+		for(let e of body){
+			e = "┃" + e + "┃".padStart(73 - e.length);
+			console.log(e);
+		}
+		console.log("┃" + " ".repeat("72") + "┃");
+		console.log("┣" + lineSeparator + "┫")
+		
+	}
+	process.stdout.write('\x1b[1A')
+	process.stdout.write('\x1b[2K') 
+	console.log("┗" + lineSeparator + "┛");
+	
+}
+
+function getCheat(getArgs){
+	const command = getArgs() ; 
+	fs.readFile(`cheats/en/${command.type}.json`,(err,data) => {
+		if(err) {} 
+
+			const bigList = JSON.parse(data.toString());
+			displayCheat(command.type,command.value,bigList[command.value]);
+
+	});
+}
+
+getCheat(getArguments);
